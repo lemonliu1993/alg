24939eedd8666cf8f67c8935c32b3d9ff750de73
lemonliu1993 <2305247091@qq.com>
2023-02-04 23:17:05 +0800

    L007_reverse_integer

---
+++ leetcode/src/main/java/com/lemon/daily/t100/L007_reverse_integer.java
@@ -0,0 +1,20 @@
+package com.lemon.daily.t100;
+
+/**
+ * Created by lemoon on 2023/2/4 23:16
+ */
+public class L007_reverse_integer {
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result) {
+                return 0;
+            }
+            result = newResult;
+            x = x / 10;
+        }
+        return result;
+    }
+}
