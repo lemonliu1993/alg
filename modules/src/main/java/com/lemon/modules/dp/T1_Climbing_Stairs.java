a2483d57800c2eaf1836d74035b4be6d6fb44c80
lemonliu1993 <2305247091@qq.com>
2023-07-06 09:32:28 +0800

    dp——T1_Climbing_Stairs

---
+++ modules/src/main/java/com/lemon/modules/dp/T1_Climbing_Stairs.java
@@ -0,0 +1,20 @@
+package com.lemon.modules.dp;
+
+/**
+ * Created by lemoon on 2023/7/5 23:03
+ */
+public class T1_Climbing_Stairs {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
