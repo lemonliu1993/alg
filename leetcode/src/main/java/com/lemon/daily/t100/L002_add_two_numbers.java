78c1c1d1c16129d638c7a27ad89b921fc168d151
lemonliu1993 <2305247091@qq.com>
2023-12-19 23:48:44 +0800

    leetcode2:两数相加

--- leetcode/src/main/java/com/lemon/daily/t100/L002_add_two_numbers.java
+++ leetcode/src/main/java/com/lemon/daily/t100/L002_add_two_numbers.java
@@ -1,6 +1,9 @@
 package com.lemon.daily.t100;
 
 /**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  * Created by lemoon on 2023/1/27 08:30
  */
 public class L002_add_two_numbers {
@@ -8,20 +11,19 @@ public class L002_add_two_numbers {
         ListNode result = new ListNode(0);
         ListNode node = result;
         int isOver = 0;
-        while (l1 != null || l2 != null || isOver > 0) {
-            int val1 = 0;
-            int val2 = 0;
+        while (l1 != null || l2 != null || isOver == 1) {
+            int num1 = 0, num2 = 0;
             if (l1 != null) {
-                val1 = l1.val;
+                num1 = l1.val;
                 l1 = l1.next;
             }
             if (l2 != null) {
-                val2 = l2.val;
+                num2 = l2.val;
                 l2 = l2.next;
             }
-            int sum = val1 + val2 + isOver;
-            isOver = sum / 10;
-            node.next = new ListNode(sum % 10);
+            int sum = (num1 + num2 + isOver) % 10;
+            isOver = (num1 + num2 + isOver) / 10;
+            node.next = new ListNode(sum);
             node = node.next;
         }
         return result.next;
