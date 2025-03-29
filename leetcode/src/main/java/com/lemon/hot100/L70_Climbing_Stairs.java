a21bd8aea2116f45c3923df9c59969177f029242
lemonliu1993 <2305247091@qq.com>
2025-03-29 19:07:02 +0800

    hot100: 爬楼梯

---
+++ leetcode/src/main/java/com/lemon/hot100/L70_Climbing_Stairs.java
@@ -0,0 +1,19 @@
+package com.lemon.hot100;
+
+/**
+ * Created by lemoon on 2025/3/29 19:05
+ */
+public class L70_Climbing_Stairs {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
