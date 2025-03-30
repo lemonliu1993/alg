fba673bd29daba5f6ba04fc7bd3a9ffebae58a15
lemonliu1993 <2305247091@qq.com>
2025-03-30 10:19:29 +0800

    hot100: 二叉树的中序遍历

---
+++ leetcode/src/main/java/com/lemon/hot100/L94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,44 @@
+package com.lemon.hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lemoon on 2025/3/30 10:15
+ */
+public class L94_Binary_Tree_Inorder_Traversal {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+
+}
