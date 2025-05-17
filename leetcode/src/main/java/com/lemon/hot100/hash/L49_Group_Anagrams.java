5b2d9b43fcd450fe412e16f2ee6650967fa204a8
lemonliu1993 <2305247091@qq.com>
2025-05-17 11:00:44 +0800

    hot100-hash:字母异位词分组

---
+++ leetcode/src/main/java/com/lemon/hot100/hash/L49_Group_Anagrams.java
@@ -0,0 +1,42 @@
+package com.lemon.hot100.hash;
+
+import java.util.*;
+
+/**
+ * 字母异位词分组
+ * Created by lemoon on 2025/5/15 08:44
+ */
+public class L49_Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return null;
+        }
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap();
+        for (int i = 0; i < strs.length; i++) {
+            char[] array = strs[i].toCharArray();
+            Arrays.sort(array);
+            //PS:这里不能用toString()方法，调用的是Object的toString
+//            String sortedStr = array.toString();
+            String sortedStr = new String(array);
+            if (map.containsKey(sortedStr)) {
+                List<String> stringList = map.get(sortedStr);
+                stringList.add(strs[i]);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(sortedStr, list);
+            }
+        }
+
+        for (List<String> list : map.values()) {
+            result.add(list);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(new L49_Group_Anagrams().groupAnagrams(strs));
+    }
+}
